881c455448178850e18b29239313911c862875d9
Wayne DSouza <info@onlinemarketingconsultants.in>
2023-04-07 16:42:01 +0530

    removed tests

--- classifier/ftuning.py
+++
@@ -1,5 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-#model_id = "sentence-transformers/all-MiniLM-L6-v2"
-model_id = "sentence-transformers/all-roberta-large-v1"
-model = SentenceTransformer(model_id)
